e7f28f2f64a65aa210976f289a180d92a9d43276
Ryan DeFever <r.s.defever@gmail.com>
2020-07-20 10:42:13 -0400

    Add signac project to run Cassandra adsorption

---
+++ simulations/adsorption/3x3x1.6nm_3-layer/cassandra/init.py
@@ -0,0 +1,44 @@
+import signac
+import numpy as np
+import unyt as u
+
+
+def init_project():
+
+    # Initialize project
+    project = signac.init_project("a3x3x1-6nm_3-layer")
+
+    # Define temperature
+    temperature = 298.0 * u.K
+    # Define chemical potentials
+    mus = [
+        -54.0 * u.kJ / u.mol,
+        -51.0 * u.kJ / u.mol,
+        -48.0 * u.kJ / u.mol,
+        -46.5 * u.kJ / u.mol,
+        -45.0 * u.kJ / u.mol,
+        -44.625 * u.kJ / u.mol,
+        -44.25 * u.kJ / u.mol,
+        -43.5 * u.kJ / u.mol,
+        -42.0 * u.kJ / u.mol,
+    ]
+
+    # Run for 150 M steps
+    nsteps = 150000000
+
+    for mu in mus:
+        for run in range(3):
+            # Define the state point
+            state_point = {
+                "T": float(temperature.in_units(u.K).value),
+                "mu": float(mu.in_units(u.kJ / u.mol).value),
+                "nsteps": nsteps,
+                "run": run,
+            }
+
+            job = project.open_job(state_point)
+            job.init()
+
+
+if __name__ == "__main__":
+    init_project()
