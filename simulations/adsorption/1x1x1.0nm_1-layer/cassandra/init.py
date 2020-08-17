62e6902d6b63fae53f74086c85db1b26609d1ec4
Ryan DeFever <r.s.defever@gmail.com>
2020-08-17 15:16:23 -0400

    Add small systems for possible CP2k comparison

---
+++ simulations/adsorption/1x1x1.0nm_1-layer/cassandra/init.py
@@ -0,0 +1,42 @@
+import signac
+import numpy as np
+import unyt as u
+
+
+def init_project():
+
+    # Initialize project
+    project = signac.init_project("a1x1x1nm_1-layer")
+
+    # Define temperature
+    temperature = 298.0 * u.K
+    # Define chemical potentials
+    mus = [
+        -48.0 * u.kJ / u.mol,
+        -47.0 * u.kJ / u.mol,
+        -46.0 * u.kJ / u.mol,
+        -45.0 * u.kJ / u.mol,
+        -44.0 * u.kJ / u.mol,
+        -43.0 * u.kJ / u.mol,
+        -42.0 * u.kJ / u.mol,
+    ]
+
+    # Run for 150 M steps
+    nsteps = 150000000
+
+    for mu in mus:
+        for run in range(3):
+            # Define the state point
+            state_point = {
+                "T": float(temperature.in_units(u.K).value),
+                "mu": float(mu.in_units(u.kJ / u.mol).value),
+                "nsteps": nsteps,
+                "run": run,
+            }
+
+            job = project.open_job(state_point)
+            job.init()
+
+
+if __name__ == "__main__":
+    init_project()
