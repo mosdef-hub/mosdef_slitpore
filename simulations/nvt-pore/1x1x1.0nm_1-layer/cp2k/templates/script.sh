9d51f63191aee6675e227bbb56d6778d4df9e63b
Ramanish Singh <singh891@ln1001.mangi.msi.umn.edu>
2020-09-22 13:34:59 -0500

    add analysis.pbs to run analysis.py on cluster

--- simulations/nvt-pore/1x1x1.0nm_1-layer/cp2k/templates/script.sh
+++ simulations/nvt-pore/1x1x1.0nm_1-layer/cp2k/templates/script.sh
@@ -6,7 +6,7 @@ module load intel/cluster/2018
 module load mkl
 module load fftw
 module load conda
-source activate /home/siepmann/singh891/anaconda3/envs/mosdef36
+source activate /home/siepmann/singh891/anaconda3/envs/slitpore37
 date >> execution.log
 {{ super() }}
 {% endblock %}
