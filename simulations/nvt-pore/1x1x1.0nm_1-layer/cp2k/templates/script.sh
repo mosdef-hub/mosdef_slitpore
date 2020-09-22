abe2139a4b729376a08e9506653029356a34e9fa
Ramanish Singh <singh891@ln1001.mangi.msi.umn.edu>
2020-09-22 12:26:46 -0500

    add templates folder cp2k

---
+++ simulations/nvt-pore/1x1x1.0nm_1-layer/cp2k/templates/script.sh
@@ -0,0 +1,13 @@
+{% extends base_script %}
+{% block project_header %}
+#PBS -l walltime=95:59:00,mem=100gb
+module purge
+module load intel/cluster/2018
+module load mkl
+module load fftw
+module load conda
+source activate /home/siepmann/singh891/anaconda3/envs/mosdef36
+date >> execution.log
+{{ super() }}
+{% endblock %}
+
