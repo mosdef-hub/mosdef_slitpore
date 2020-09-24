3b9bea3558bd90c927899ab89cb476665fea0cb1
Ryan DeFever <r.s.defever@gmail.com>
2020-09-24 12:05:59 -0400

    Update 3x3x2.0nm NVT simulations

--- simulations/nvt-pore/3x3x2.0nm_3-layer/cassandra/analysis/analyze.py
+++ simulations/nvt-pore/3x3x2.0nm_3-layer/cassandra/analysis/analyze.py
@@ -8,9 +8,9 @@ from mosdef_slitpore.utils.cassandra import spce_water
 from mosdef_slitpore.analysis import compute_density
 from mosdef_slitpore.analysis import compute_s
 
+
 def main():
 
-    nwaters=[485, 490, 495, 500, 505]
     project = signac.get_project("../")
 
     empty_pore = mbuild.recipes.GraphenePore(
@@ -29,9 +29,10 @@ def main():
     single_water = spce_water()
     single_water.name = 'SOL'
 
-    df = pd.DataFrame()
+    for nwater, group in project.groupby("nwater"):
+        # New dataframe to save the results
+        df = pd.DataFrame()
 
-    for nwater in nwaters:
         # Create a topology for the given system
         water_box = mbuild.Box([3.0, 3.0, 1.8]) #nm
         water = mbuild.fill_box(single_water, n_compounds=nwater, box=water_box)
@@ -43,10 +44,9 @@ def main():
         xy_area = filled_pore.periodicity[0] * filled_pore.periodicity[1]
         top = filled_pore.to_trajectory(residues=["RES", "SOL"])
 
-        # Load 3 trajectories and combine
-        jobs = project.find_jobs({"nwater": nwater})
-        trajs = []
-        for job in jobs:
+        # Load all trajectories and combine
+        for job in group:
+            run = job.sp.run
             # Load in full trajectory
             full_traj = md.load(job.fn("prod.nvt.out.xyz"), top=top)
             # Add unit cell information
@@ -58,30 +58,36 @@ def main():
             )
             # Keep only water
             slice_water = full_traj.top.select("water and name O H")
-            trajs.append(full_traj.atom_slice(slice_water))
-        # Combine in "1" long trajectory to analyze
-        traj = md.join(trajs)
-        slice_ow = traj.top.select("name O")
-        slice_hw = traj.top.select("name H")
-        traj_ow = traj.atom_slice(slice_ow)
-        traj_hw = traj.atom_slice(slice_hw)
+            traj = full_traj.atom_slice(slice_water)
+            slice_ow = traj.top.select("name O")
+            slice_hw = traj.top.select("name H")
+            traj_ow = traj.atom_slice(slice_ow)
+            traj_hw = traj.atom_slice(slice_hw)
 
-        # Compute the density
-        bin_centers_ow, density_ow = compute_density(traj_ow, xy_area)
-        bin_centers_hw, density_hw = compute_density(traj_hw, xy_area)
+            # Compute the density
+            bin_centers_ow, density_ow = compute_density(traj_ow, xy_area, bin_width=0.005)
+            bin_centers_hw, density_hw = compute_density(traj_hw, xy_area, bin_width=0.005)
 
-        # Compute the s order parameter
-        bin_centers_s, s_results = compute_s(traj)
+            # Compute the s order parameter
+            bin_centers_s, s_results = compute_s(traj, bin_width=0.005)
 
-        assert np.allclose(bin_centers_ow, bin_centers_hw)
-        assert np.allclose(bin_centers_ow, bin_centers_s)
+            assert np.allclose(bin_centers_ow, bin_centers_hw)
+            assert np.allclose(bin_centers_ow, bin_centers_s)
 
-        # Save results
-        df[f"z-loc_nm"] = bin_centers_ow
-        df[f"density-ow_nm^-3"] = density_ow
-        df[f"density-hw_nm^-3"] = density_hw
-        df[f"s_value"] = s_results
-        df.to_csv(f"results_nd_{nwater}-water.csv")
+            # Save results
+            tmp_df = pd.DataFrame()
+            tmp_df[f"run"] = run
+            tmp_df[f"z-loc_nm"] = bin_centers_ow
+            tmp_df[f"density-ow_nm^-3"] = density_ow
+            tmp_df[f"density-hw_nm^-3"] = density_hw
+            tmp_df[f"s_value"] = s_results
+            df = df.append(tmp_df)
+
+        # Compute mean/stdev and save to file
+        means = df.groupby("z-loc_nm").mean().drop(columns=["run"]).add_suffix("_mean")
+        stds = df.groupby("z-loc_nm").std().drop(columns=["run"]).add_suffix("_std")
+        combined = means.merge(stds, on="z-loc_nm")
+        combined.to_csv(f"results_nd_{nwater}-water.csv")
 
 
 if __name__ == "__main__":
