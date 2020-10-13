105aa2da6410b0f0f58c85e1efb3cc3131bd9c12
Ray Matsumoto <raymat@master.cl.vanderbilt.edu>
2020-10-13 08:25:05 -0400

    Add data and updated initialization script for 1 water system

--- simulations/nvt-pore/1x1x1.0nm_1-layer/gromacs/init.py
+++ simulations/nvt-pore/1x1x1.0nm_1-layer/gromacs/init.py
@@ -11,7 +11,7 @@ def init_project():
     temperature = 298.0 * u.K
 
     # Start with a few different number of waters in the pore
-    nwaters = [23, 24]
+    nwaters = [1, 23, 24]
 
     for nwater in nwaters:
         # Define the state point
