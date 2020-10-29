580c6b4ec34d1213dff9712e8300d2314efcf6d8
Ray Matsumoto <raymat@master.cl.vanderbilt.edu>
2020-10-29 14:28:11 -0400

    Adjust histograms so sum equals 1

--- simulations/nvt-pore/1x1x1.0nm_1-layer/gromacs/angle_distribution.py
+++ simulations/nvt-pore/1x1x1.0nm_1-layer/gromacs/angle_distribution.py
@@ -63,7 +63,7 @@ def angle_dist(job):
     all_cos_angle_list = np.asarray(all_cos_angle_list)
     all_angle_list = (180 / np.pi) * np.arccos(all_cos_angle_list)
     counts, cos_angle_bins, bars = plt.hist(
-        all_cos_angle_list, bins=60, alpha=0.5, density=True
+        all_cos_angle_list, bins=60, alpha=0.5, density=False
     )
     cos_angle_bins_center = (cos_angle_bins[:-1] + cos_angle_bins[1:]) / 2
     plt.xlabel("cos(angle)")
@@ -85,8 +85,10 @@ def angle_dist(job):
             header="cos_Angle_bins\tRelativeFreq",
         )
     plt.figure()
+    bins = np.linspace(0, 180, num=61)
+    weights = np.ones_like(all_angle_list) / len(all_angle_list)
     counts, angle_bins, bars = plt.hist(
-        all_angle_list, bins=np.linspace(0, 180, num=61), alpha=0.5, density=True
+        all_angle_list, bins=bins, weights=weights, alpha=0.5
     )
     plt.xlabel("Angle (degress)")
     plt.ylabel("Relative frequency")
@@ -110,11 +112,12 @@ def angle_dist(job):
         )
 
     plt.figure()
+    weights = np.ones_like(angle_bins_center) / len(angle_bins_center)
     normalized_counts = np.divide(
         counts, abs(np.sin((np.pi / 180) * angle_bins_center))
     )
-    arr = plt.hist(angle_bins_center, weights=normalized_counts, bins=np.linspace(0, 180, num=61), density=True)
-    # plt.bar(angle_bins_center,normalized_counts)
+    normalized_counts /= np.sum(normalized_counts)
+    arr = plt.hist(angle_bins_center, weights=normalized_counts, bins=bins, density=False)
     plt.xlabel("Angle (degress)")
     plt.ylabel("Relative frequency")
     plt.title("Normalized distribution")
