0e0b3af815f7be8caa66e24fd84ad7be4b778d08
Ryan DeFever <r.s.defever@gmail.com>
2020-12-18 14:21:57 -0500

    Fix packaging issues for non-editable install

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-setup(name='mosdef_slitpore',
-        version='0.0',
-        description='routines and functions for graphene slitpore simulations',
-        author='Ray Matsumoto',
-        author_email='ray.a.matsumoto@vanderbilt.edu',
-        license='MIT',
-        packages=['mosdef_slitpore'],
-        zip_safe=False,
-        )
+setup(
+    name='mosdef_slitpore',
+    version='0.0',
+    description='routines and functions for graphene slitpore simulations',
+    author='Ray Matsumoto',
+    author_email='ray.a.matsumoto@vanderbilt.edu',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+)
